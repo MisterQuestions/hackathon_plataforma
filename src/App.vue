ed80823fd6e50eb3f1183681e229252066897786
MisterQuestions <misterquestions.mx@gmail.com>
2019-12-08 01:14:23 -0600

    Recreated project and added Vuex

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,32 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <router-view />
-=======
-    <!--div id="nav">
+    <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div-->
+    </div>
     <router-view/>
->>>>>>> a27e93e202954bd88e975e5c616af77930efd15b
   </div>
 </template>
 
 <style lang="scss">
-<<<<<<< HEAD
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  position: relative;
   color: #2c3e50;
 }
-=======
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
 
-body {
-  background-color: #F2F4F4;
-  font-family: 'Roboto', sans-serif;
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
->>>>>>> a27e93e202954bd88e975e5c616af77930efd15b
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from '../views/Login.vue'
-<<<<<<< HEAD
-import Register from '../views/Register.vue'
-=======
-import Dashboard from '../views/Dashboard.vue'
->>>>>>> a27e93e202954bd88e975e5c616af77930efd15b
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'login',
-    component: Login
-  },
-  {
-<<<<<<< HEAD
-    path: '/register',
-    name: 'register',
-    component: Register
+    name: 'home',
+    component: Home
   },
   {
     path: '/about',
@@ -28,23 +17,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-=======
-    path: '/dashboard',
-    name: 'dashboard',
-    component: Dashboard
->>>>>>> a27e93e202954bd88e975e5c616af77930efd15b
   }
-  // {
-  //  path: '/about',
-  //  name: 'about',
-  // route level code-splitting
-  // this generates a separate chunk (about.[hash].js) for this route
-  // which is lazy-loaded when the route is visited.
-  //  component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes
 })
 
