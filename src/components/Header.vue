1ea3f39cf2abf1e9f1373ca11534c3ba3b50fed4
Fernando Lopez <misterquestions.mx@gmail.com>
2019-12-08 10:57:14 -0600

    Fixed various issues with files

---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="header">
+    <div class="me">
+      <div class="name">Nombre</div>
+      <div class="group"><b>Group</b></div>
+    </div>
+    <div class="quickactions">
+      <div class="icon notifications">
+        <i class="material-icons">notifications</i>
+      </div>
+      <div class="icon messages">
+        <i class="material-icons">email</i>
+      </div>
+      <div class="icon profile">
+        <i class="material-icons">person</i>
+      </div>
+      <div class="icon">
+        <i class="material-icons logout">input</i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  background-color: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px;
+}
+
+.me {
+  margin-left: 25px;
+}
+
+.quickactions {
+  display: flex;
+  align-items: center;
+}
+
+i {
+  color: #ffffff;
+}
+
+.icon {
+  width: 35px;
+  height: 35px;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 5px;
+}
+
+.notifications {
+  background-color: #28b3b8;
+}
+
+.messages {
+  background-color: #0b5659;
+}
+
+.profile {
+  background-color: #dbb418;
+}
+
+.logout {
+  color: #ff2e2e;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,35 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'login',
+    component: Login
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/register',
+    name: 'register',
+    component: Register
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
   }
+  // {
+  // path: '/about',
+  // name: 'about',
+  // route level code-splitting
+  // this generates a separate chunk (about.[hash].js) for this route
+  // which is lazy-loaded when the route is visited.
+  // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // }
 ]
 
 const router = new VueRouter({
